25f87aaed99c461301b9861482a2136a608e8f3e
Praman Bhogal <pramanbhogal13@gmail.com>
2026-05-28 18:37:07 +0530

    added User model

---
+++ src/models/users.model.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    }
+}, { timestamps: true });
+
+const userModel = mongoose.model("user", userSchema);
+
+export default userModel;
